0ecb68c0ca8c9f059c11b1b2a7ca94edb3f52401
Jessica <jzwang@mit.edu>
2014-05-30 17:55:00 -0400

    hello world

---
+++ stuff.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('.header').delay(500).fadeIn(2000);
+	$('.pagecontainer h3').delay(1000).animate({
+		opacity: .9,
+		right: "+=10px",
+	}, 1000);
+
+	$('.pagecontainer h5').delay(1000).animate({
+		opacity: .9,
+		left: "+=5px",
+	}, 1000);
+
+	$('.nav').delay(500).fadeIn(2000);
+		$('.nav li').delay(1000).animate({
+		opacity: .9,
+		right: "+=10px",
+	}, 1000);
+})
+
+// $(document).ready(function(){
+
+
+
+// })
